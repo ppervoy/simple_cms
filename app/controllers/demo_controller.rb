788143a94c64e9a6345a8aa9bddf33f7eb510705
Pavel Pervoy <p.pervoy@gmail.com>
2014-11-06 22:57:07 +0200

    05 01 rendering templates

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,12 @@ class DemoController < ApplicationController
 	layout false
 
 	def index
+		#render(:template => 'demo/hello')
+		#render('demo/hello')
+		render('hello')
+	end
+
+	def hello
+		render('index')
 	end
 end
