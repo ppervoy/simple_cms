6d2aa673c6be54f636d4ec147feb4757d117ef91
Pavel Pervoy <p.pervoy@gmail.com>
2014-11-06 23:06:54 +0200

    05 02 redirect actions

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -11,4 +11,13 @@ class DemoController < ApplicationController
 	def hello
 		render('index')
 	end
+
+	def other_hello
+		#redirect_to(:controller => 'demo', :action => 'index')
+		redirect_to(:action => 'index')
+	end
+
+	def google
+		redirect_to("http://google.com")
+	end
 end
