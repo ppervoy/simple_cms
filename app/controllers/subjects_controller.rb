624f191f2e5540fce432f2a0ed51ddde90676a14
Pavel Pervoy <p.pervoy@gmail.com>
2014-11-09 20:51:07 +0200

    09 08 CDUD edit

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -23,7 +23,18 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])  
   end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+      redirect_to(:action=>'show', :id=>params[:id])
+    else
+      render('edit')
+    end
+  end 
+
 
   def delete
   end
