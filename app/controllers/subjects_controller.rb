d77f6a3e7edb13aa382cbe3d8ff82d12b948dbb7
Pavel Pervoy <p.pervoy@gmail.com>
2014-11-09 21:19:19 +0200

    09 10 flashhash

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -16,6 +16,7 @@ class SubjectsController < ApplicationController
   def create
     @subject = Subject.new(subject_params)
     if @subject.save
+      flash[:notice] = "Subject created successfully"
       redirect_to(:action=>'index')
     else
       render('new')
@@ -29,6 +30,7 @@ class SubjectsController < ApplicationController
   def update
     @subject = Subject.find(params[:id])
     if @subject.update_attributes(subject_params)
+      flash[:notice] = "Subject updated successfully"
       redirect_to(:action=>'show', :id=>params[:id])
     else
       render('edit')
@@ -42,7 +44,7 @@ class SubjectsController < ApplicationController
 
   def destroy
     s = Subject.find(params[:id]).destroy
-    
+    flash[:notice] = "Subject '#{s.name}' removed successfully"
     redirect_to(:action=>'index')
   end
 
