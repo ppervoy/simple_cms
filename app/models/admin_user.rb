6bf51b81c1ba919ae9a4cfe049933725f45a5b3b
Pavel Pervoy <p.pervoy@gmail.com>
2014-12-12 01:50:16 +0200

    13_04 custom validations

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,6 +8,8 @@ class AdminUser < ActiveRecord::Base
 
 	EMAIL_REGEX = /\A[a-z)-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 
+	FORBIDDEN_USERNAMES = ["littlebopeep", "humptydumpty", "marymary"]
+
 	# validates_presence_of :first_name
 	# validates_length_of :first_name, :maximum => 25
 	validates :first_name,	:presence => true,
@@ -33,4 +35,19 @@ class AdminUser < ActiveRecord::Base
 						:length => {:maximum => 100},
 						:format => EMAIL_REGEX,
 						:confirmation => true
+
+	validate :username_is_allowed
+	#validate :no_new_users_on_saturdays, :on => :create
+
+	def username_is_allowed
+		 if FORBIDDEN_USERNAMES.include?(username)
+		 	errors.add(:username, "has been restricted from use!!! ")
+		 end
+	end
+
+	def no_new_users_on_saturdays
+		if Time.now.wday == 6
+			errors[:base] << "No new users on Saturdays!!!"
+		end
+	end
 end
