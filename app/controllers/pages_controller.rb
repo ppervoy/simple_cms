1067c2d0cb09a67872029da4efd79c1c19633396
Pavel Pervoy <p.pervoy@gmail.com>
2014-12-14 18:28:16 +0200

    15_03 netsing pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
 
   before_action :confirm_logged_in
+  before_action :find_subject
 
   layout "admin"
 
   def index
-    @pages = Page.sorted
+    #@pages = Page.where(:subject_id=>@subject.id).sorted
+    @pages = @subject.pages.sorted
   end
 
   def show
@@ -13,7 +15,7 @@ class PagesController < ApplicationController
   end
 
   def new
-    @page = Page.new({:name => "Default"})
+    @page = Page.new({:subject_id => @subject.id, :name => "Default"})
     @subjects = Subject.order('position ASC')
     @page_count = Page.count + 1
   end
@@ -22,7 +24,7 @@ class PagesController < ApplicationController
     @page = Page.new(page_params)
     if @page.save
       flash[:notice] = "Page created successfully"
-      redirect_to(:action => "index")
+      redirect_to(:action => "index", :subject_id=> @subject.id)
     else
       @subjects = Subject.order('position ASC')
       @page_count = Page.count + 1
@@ -40,7 +42,7 @@ class PagesController < ApplicationController
     @page = Page.find(params[:id])
     if @page.update_attributes(page_params)
       flash[:notice] = "Page updated successfully"
-      redirect_to(:action => "show", :id => @page.id)
+      redirect_to(:action => "show", :id => @page.id, :subject_id => @subject.id)
     else
       @subjects = Subject.order('position ASC')
       @page_count = Page.count
@@ -55,12 +57,18 @@ class PagesController < ApplicationController
   def destroy
     page = Page.find(params[:id]).destroy
     flash[:notice] = "Page destroyed"
-    redirect_to(:action => "index")
+    redirect_to(:action => "index", :subject_id => @subject.id)
   end
 
   private
 
   def page_params
     params.require(:page).permit(:subject_id, :name, :permalink, :position, :visible)
+  end
+
+  def find_subject
+    if params[:subject_id]
+      @subject = Subject.find(params[:subject_id])
+    end
   end
 end
